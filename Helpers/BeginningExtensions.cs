670a9e53a55023ec85bf3bf58369e6312111c762
Andy Walter <musictopia2@hotmail.com>
2023-04-17 13:28:38 -0700

    Changed the extensions from publish to publishall Trying to fix the issue for the duplicate bug. Of course, if this causes other issues, then would have to discard the entire mvvm system.

--- Helpers/BeginningExtensions.cs
+++ Helpers/BeginningExtensions.cs
@@ -3,15 +3,15 @@ public static class BeginningExtensions
 {
     public static void Ask(this IEventAggregator aggregator, Type type)
     {
-        aggregator.Publish(new AskEventModel(), type.Name);
+        aggregator.PublishAll(new AskEventModel(), type.Name); //try publishall to attempt to avoid the duplicate problem (could cause to be freaky but the risk i have to take)
     }
     public static void OpenView(this IScreen screen)
     {
         OpenEventModel open = new(screen);
-        screen.Aggregator.Publish(open, screen.GetType().Name);
+        screen.Aggregator.PublishAll(open, screen.GetType().Name);
     }
     public static void CloseView(this IScreen screen)
     {
-        screen.Aggregator.Publish(new CloseEventModel(), screen.GetType().Name);
+        screen.Aggregator.PublishAll(new CloseEventModel(), screen.GetType().Name); //trying to do publishall for all three for the mvvm framework.  taking a huge risk.  since its used for one specific purpose, testing it.
     }
 }
